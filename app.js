dc9225b9087abd7024b37f401afb0004f9340ec1
GunnarSon <84020169+GunnarDen@users.noreply.github.com>
2024-04-09 21:06:34 +0700

    Упражнение - Расчет проекта

---
+++ app.js
@@ -0,0 +1,6 @@
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+
+console.log('Смогу ли я работать? ' + (availableHours >= projectHours));
+console.log('Стоимость работ: ' + projectHours * payRateUSD + '$');
